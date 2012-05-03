78d59f33cc81c1cfbfb5d4920ebe2ffe13dff240
Heila van der Merwe <heilamvdm@gmail.com>
2012-05-03 12:59:09 +0200

    DeadlockExample corrections

---
+++ jpf-android/src/classes/android/app/Activity.java
@@ -0,0 +1,18 @@
+package android.app;
+
+import android.os.Bundle;
+
+public class Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setContentView(int id){
+		
+	}
+	
+}
+
+
