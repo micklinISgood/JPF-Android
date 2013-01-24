bf2b2303e0d824beea6c4c25514c2fa16701e4b3
Heila van der Merwe <heilamvdm@gmail.com>
2013-01-24 16:09:56 +0200

    adding Dialog

---
+++ jpf-android/src/classes/android/app/Dialog.java
@@ -0,0 +1,42 @@
+package android.app;
+
+import android.os.Bundle;
+
+public class Dialog {
+
+  public void dismiss() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void show() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void setOwnerActivity(Activity activity) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void dispatchOnCreate(Bundle state) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public Bundle onSaveInstanceState() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean isShowing() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void onRestoreInstanceState(Bundle dialogState) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
