34cd21fc56fb9192c4564df31c0b293d89cce098
heila <heilamvdm@gmail.com>
2013-05-29 14:46:28 +0200

    Adding Log info

--- jpf-android/src/classes/android/os/Handler.java
+++ jpf-android/src/classes/android/os/Handler.java
@@ -1,5 +1,7 @@
 package android.os;
 
+import java.lang.reflect.Modifier;
+
 import android.util.Log;
 import android.util.Printer;
 
@@ -12,182 +14,228 @@ import android.util.Printer;
  * 
  */
 public class Handler {
-	private static final String TAG = "Handler";
+  /*
+   * Set this flag to true to detect anonymous, local or member classes
+   * that extend this Handler class and that are not static. These kind
+   * of classes can potentially create leaks.
+   */
+  private static final boolean FIND_POTENTIAL_LEAKS = true;
+  private static final String TAG = "Handler";
 
-	/**
-	 * Callback interface you can use when instantiating a Handler to avoid
-	 * having to implement your own subclass of Handler.
-	 */
-	public interface Callback {
-		public boolean handleMessage(Message msg);
-	}
+  /**
+   * Callback interface you can use when instantiating a Handler to avoid
+   * having to implement your own subclass of Handler.
+   */
+  public interface Callback {
+    public boolean handleMessage(Message msg);
+  }
 
-	public Handler() {
-		mLooper = Looper.myLooper();
-		if (mLooper == null) {
-			throw new RuntimeException(
-					"Can't create handler inside thread that has not called Looper.prepare()");
-		}
-		mQueue = mLooper.mQueue;
-		mCallback = null;
-	}
+  /**
+   * Subclasses must implement this to receive messages.
+   */
+  public void handleMessage(Message msg) {
+  }
 
-	/**
-	 * Constructor associates this handler with the queue for the current thread
-	 * and takes a callback interface in which you can handle messages.
-	 */
-	public Handler(Callback callback) {
-		mLooper = Looper.myLooper();
-		if (mLooper == null) {
-			throw new RuntimeException(
-					"Can't create handler inside thread that has not called Looper.prepare()");
-		}
-		mQueue = mLooper.mQueue;
-		mCallback = callback;
-	}
+  /**
+   * Handle system messages here.
+   */
+  public void dispatchMessage(Message msg) {
+    if (msg.callback != null) {
+      handleCallback(msg);
+    } else {
+      if (mCallback != null) {
+        if (mCallback.handleMessage(msg)) {
+          return;
+        }
+      }
+      handleMessage(msg);
+    }
+  }
 
-	/**
-	 * Use the provided queue instead of the default one.
-	 */
-	public Handler(Looper looper) {
-		mLooper = looper;
-		mQueue = looper.mQueue;
-		mCallback = null;
-	}
+  /**
+   * Default constructor associates this handler with the queue for the
+   * current thread.
+   * 
+   * If there isn't one, this handler won't be able to receive messages.
+   */
+  public Handler() {
+    if (FIND_POTENTIAL_LEAKS) {
+      final Class<? extends Handler> klass = getClass();
+      if ((klass.isAnonymousClass() || klass.isMemberClass() || klass.isLocalClass())
+          && (klass.getModifiers() & Modifier.STATIC) == 0) {
+        Log.w(TAG,
+            "The following Handler class should be static or leaks might occur: " + klass.getCanonicalName());
+      }
+    }
+    mLooper = Looper.myLooper();
+    if (mLooper == null) {
+      throw new RuntimeException("Can't create handler inside thread that has not called Looper.prepare()");
+    }
+    mQueue = mLooper.mQueue;
+    mCallback = null;
+  }
 
-	/**
-	 * Use the provided queue instead of the default one and take a callback
-	 * interface in which to handle messages.
-	 */
-	public Handler(Looper looper, Callback callback) {
-		mLooper = looper;
-		mQueue = looper.mQueue;
-		mCallback = callback;
-	}
+  /**
+   * Constructor associates this handler with the queue for the current thread
+   * and takes a callback interface in which you can handle messages.
+   */
+  public Handler(Callback callback) {
+    if (FIND_POTENTIAL_LEAKS) {
+      final Class<? extends Handler> klass = getClass();
+      if ((klass.isAnonymousClass() || klass.isMemberClass() || klass.isLocalClass())
+          && (klass.getModifiers() & Modifier.STATIC) == 0) {
+        Log.w(TAG,
+            "The following Handler class should be static or leaks might occur: " + klass.getCanonicalName());
+      }
+    }
+    
+    mLooper = Looper.myLooper();
+    if (mLooper == null) {
+      throw new RuntimeException("Can't create handler inside thread that has not called Looper.prepare()");
+    }
+    mQueue = mLooper.mQueue;
+    mCallback = callback;
+  }
 
-	/**
-	 * Subclasses must implement this to receive messages.
-	 */
-	public void handleMessage(Message msg) {
-	}
+  /**
+   * Use the provided queue instead of the default one.
+   */
+  public Handler(Looper looper) {
+    mLooper = looper;
+    mQueue = looper.mQueue;
+    mCallback = null;
+  }
 
-	public void dispatchMessage(Message msg) {
-		if (msg.callback != null) {
-			handleCallback(msg);
-		} else {
-			if (mCallback != null) {
-				if (mCallback.handleMessage(msg)) {
-					return;
-				}
-			}
-			handleMessage(msg);
-		}
-	}
+  /**
+   * Use the provided queue instead of the default one and take a callback
+   * interface in which to handle messages.
+   */
+  public Handler(Looper looper, Callback callback) {
+    mLooper = looper;
+    mQueue = looper.mQueue;
+    mCallback = callback;
+  }
+  
+  /**
+   * Returns a string representing the name of the specified message.
+   * The default implementation will either return the class name of the
+   * message callback if any, or the hexadecimal representation of the
+   * message "what" field.
+   *  
+   * @param message The message whose name is being queried 
+   */
+  public String getMessageName(Message message) {
+      if (message.callback != null) {
+          return message.callback.getClass().getName();
+      }
+      return "0x" + Integer.toHexString(message.what);
+  }
+  
+  
 
-	public final boolean post(Runnable r) {
-		Message m = new Message();
-		m.callback = r;
-		return sendMessage(m);
-	}
+  public final boolean post(Runnable r) {
+    Message m = new Message();
+    m.callback = r;
+    return sendMessage(m);
+  }
 
-	public final boolean sendMessage(Message msg) {
-		// enqueue message
-		boolean sent = false;
-		MessageQueue queue = mQueue;
-		msg.target = this;
-		sent = queue.enqueueMessage(msg);
-		return sent;
-	}
+  public final boolean sendMessage(Message msg) {
+    // enqueue message
+    boolean sent = false;
+    MessageQueue queue = mQueue;
+    msg.target = this;
+    sent = queue.enqueueMessage(msg);
+    return sent;
+  }
 
-	public final boolean sendStopMessage(Message msg) {
-		// enqueue message
-		boolean sent = false;
-		MessageQueue queue = mQueue;
-		sent = queue.enqueueMessage(msg);
-		return sent;
-	}
+  public final boolean sendStopMessage(Message msg) {
+    // enqueue message
+    boolean sent = false;
+    MessageQueue queue = mQueue;
+    sent = queue.enqueueMessage(msg);
+    return sent;
+  }
 
-	/**
-	 * Enqueue a message at the front of the message queue, to be processed on
-	 * the next iteration of the message loop. You will receive it in
-	 * {@link #handleMessage}, in the thread attached to this handler. <b>This
-	 * method is only for use in very special circumstances -- it can easily
-	 * starve the message queue, cause ordering problems, or have other
-	 * unexpected side-effects.</b>
-	 * 
-	 * @return Returns true if the message was successfully placed in to the
-	 *         message queue. Returns false on failure, usually because the
-	 *         looper processing the message queue is exiting.
-	 */
-	public final boolean sendMessageAtFrontOfQueue(Message msg) {
-		boolean sent = false;
-		MessageQueue queue = mQueue;
-		if (queue != null) {
-			msg.target = this;
-			sent = queue.enqueueMessage(msg, 0);
-		} else {
-			RuntimeException e = new RuntimeException(this
-					+ " sendMessageAtTime() called with no mQueue");
-			Log.w("Looper", e.getMessage(), e);
-		}
-		return sent;
-	}
+  /**
+   * Enqueue a message at the front of the message queue, to be processed on
+   * the next iteration of the message loop. You will receive it in
+   * {@link #handleMessage}, in the thread attached to this handler. <b>This
+   * method is only for use in very special circumstances -- it can easily
+   * starve the message queue, cause ordering problems, or have other
+   * unexpected side-effects.</b>
+   * 
+   * @return Returns true if the message was successfully placed in to the
+   *         message queue. Returns false on failure, usually because the
+   *         looper processing the message queue is exiting.
+   */
+  public final boolean sendMessageAtFrontOfQueue(Message msg) {
+    boolean sent = false;
+    MessageQueue queue = mQueue;
+    if (queue != null) {
+      msg.target = this;
+      sent = queue.enqueueMessage(msg, 0);
+    } else {
+      RuntimeException e = new RuntimeException(this + " sendMessageAtTime() called with no mQueue");
+      Log.w("Looper", e.getMessage(), e);
+    }
+    return sent;
+  }
 
-	public final Looper getLooper() {
-		return mLooper;
-	}
+  public final Looper getLooper() {
+    return mLooper;
+  }
 
-	public final void dump(Printer pw, String prefix) {
-		pw.println(prefix + this + " @ " + SystemClock.uptimeMillis());
-		if (mLooper == null) {
-			pw.println(prefix + "looper uninitialized");
-		} else {
-			mLooper.dump(pw, prefix + "  ");
-		}
-	}
+  public final void dump(Printer pw, String prefix) {
+    pw.println(prefix + this + " @ " + SystemClock.uptimeMillis());
+    if (mLooper == null) {
+      pw.println(prefix + "looper uninitialized");
+    } else {
+      mLooper.dump(pw, prefix + "  ");
+    }
+  }
 
-	@Override
-	public String toString() {
-		return "Handler (" + getClass().getName() + ") {"
-				+ Integer.toHexString(System.identityHashCode(this)) + "}";
-	}
+  @Override
+  public String toString() {
+    return "Handler (" + getClass().getName() + ") {" + Integer.toHexString(System.identityHashCode(this))
+        + "}";
+  }
 
-	// final IMessenger getIMessenger() {
-	// synchronized (mQueue) {
-	// if (mMessenger != null) {
-	// return mMessenger;
-	// }
-	// mMessenger = new MessengerImpl();
-	// return mMessenger;
-	// }
-	// }
-	//
-	// private final class MessengerImpl extends IMessenger.Stub {
-	// public void send(Message msg) {
-	// Handler.this.sendMessage(msg);
-	// }
-	// }
+  // final IMessenger getIMessenger() {
+  // synchronized (mQueue) {
+  // if (mMessenger != null) {
+  // return mMessenger;
+  // }
+  // mMessenger = new MessengerImpl();
+  // return mMessenger;
+  // }
+  // }
+  //
+  // private final class MessengerImpl extends IMessenger.Stub {
+  // public void send(Message msg) {
+  // Handler.this.sendMessage(msg);
+  // }
+  // }
 
-	private final Message getPostMessage(Runnable r) {
-		Message m = new Message();
-		m.callback = r;
-		return m;
-	}
+  private final Message getPostMessage(Runnable r) {
+    Message m = new Message();
+    m.callback = r;
+    return m;
+  }
 
-	private final Message getPostMessage(Runnable r, Object token) {
-		Message m = new Message();
-		m.obj = token;
-		m.callback = r;
-		return m;
-	}
+  private final Message getPostMessage(Runnable r, Object token) {
+    Message m = new Message();
+    m.obj = token;
+    m.callback = r;
+    return m;
+  }
 
-	private final void handleCallback(Message message) {
-		message.callback.run();
-	}
+  private final void handleCallback(Message message) {
+    message.callback.run();
+  }
 
-	final MessageQueue mQueue;
-	final Looper mLooper;
-	final Callback mCallback;
-	// IMessenger mMessenger;
+  final MessageQueue mQueue;
+  final Looper mLooper;
+  final Callback mCallback;
+  // IMessenger mMessenger;
 
 }
--- jpf-android/src/classes/android/os/Looper.java
+++ jpf-android/src/classes/android/os/Looper.java
@@ -1,5 +1,7 @@
 package android.os;
 
+import android.util.Log;
+import android.util.LogPrinter;
 import android.util.Printer;
 
 /**
@@ -9,194 +11,195 @@ import android.util.Printer;
  * 
  */
 public class Looper {
-	private static final String TAG = "Looper";
+  private static final String TAG = "Looper";
+  private static final boolean DEBUG_LOOPER = false;
 
-	// sThreadLocal.get() will return null unless you've called prepare().
-	static final ThreadLocal<Looper> sThreadLocal = new ThreadLocal<Looper>();
-	
-	//List<Handler> handlers; Keep reference to all handlers
-	final MessageQueue mQueue;
-	final Thread mThread;
-	volatile boolean mRun;
+  // sThreadLocal.get() will return null unless you've called prepare().
+  static final ThreadLocal<Looper> sThreadLocal = new ThreadLocal<Looper>();
 
-	private Printer mLogging = null;
-	private static Looper mMainLooper = null; // guarded by Looper.class
+  //List<Handler> handlers; Keep reference to all handlers
+  final MessageQueue mQueue;
+  final Thread mThread;
+  volatile boolean mRun;
+  private Printer mLogging;
 
-	/**
-	 * Initialize the current thread as a looper. This gives you a chance to
-	 * create handlers that then reference this looper, before actually starting
-	 * the loop. Be sure to call {@link #loop()} after calling this method, and
-	 * end it by calling {@link #quit()}.
-	 */
-	public static void prepare() {
-		if (sThreadLocal.get() != null) {
-			throw new RuntimeException(
-					"Only one Looper may be created per thread");
-		}
-		sThreadLocal.set(new Looper());
-	}
+  private static Looper mMainLooper = null; // guarded by Looper.class
 
-	/**
-	 * Initialize the current thread as a looper, marking it as an application's
-	 * main looper. The main looper for your application is created by the
-	 * Android environment, so you should never need to call this function
-	 * yourself. See also: {@link #prepare()}
-	 */
-	public static void prepareMainLooper() {
-		prepare();
-		setMainLooper(myLooper());
-		myLooper().mQueue.mQuitAllowed = false;
-	}
+  /**
+   * Initialize the current thread as a looper. This gives you a chance to
+   * create handlers that then reference this looper, before actually starting
+   * the loop. Be sure to call {@link #loop()} after calling this method, and
+   * end it by calling {@link #quit()}.
+   */
+  public static void prepare() {
+    if (sThreadLocal.get() != null) {
+      throw new RuntimeException("Only one Looper may be created per thread");
+    }
+    sThreadLocal.set(new Looper());
+  }
 
-	private synchronized static void setMainLooper(Looper looper) {
-		mMainLooper = looper;
-	}
+  /**
+   * Initialize the current thread as a looper, marking it as an application's
+   * main looper. The main looper for your application is created by the
+   * Android environment, so you should never need to call this function
+   * yourself. See also: {@link #prepare()}
+   */
+  public static void prepareMainLooper() {
+    Log.i(TAG, "Preparing Looper");
+    prepare();
+    setMainLooper(myLooper());
+    myLooper().mQueue.mQuitAllowed = false;
+  }
 
-	/**
-	 * Returns the application's main looper, which lives in the main thread of
-	 * the application.
-	 */
-	public synchronized static Looper getMainLooper() {
-		return mMainLooper;
-	}
+  private synchronized static void setMainLooper(Looper looper) {
+    mMainLooper = looper;
+  }
 
-	/**
-	 * Run the message queue in this thread. Be sure to call {@link #quit()} to
-	 * end the loop.
-	 */
-	public static void loop() {
-		Looper me = myLooper();
-		if (me == null) {
-			throw new RuntimeException(
-					"No Looper; Looper.prepare() wasn't called on this thread.");
-		}
-		MessageQueue queue = me.mQueue;
+  /**
+   * Returns the application's main looper, which lives in the main thread of
+   * the application.
+   */
+  public synchronized static Looper getMainLooper() {
+    return mMainLooper;
+  }
 
-		// Make sure the identity of this thread is that of the local process,
-		// and keep track of what that identity token actually is.
-		// Binder.clearCallingIdentity();
-		// final long ident = Binder.clearCallingIdentity();
+  /**
+   * Run the message queue in this thread. Be sure to call {@link #quit()} to
+   * end the loop.
+   */
+  public static void loop() {
+    Log.i(TAG, "Starting Looper");
 
-		while (true) {
-			// This must be in a local variable, in case a UI event sets the
-			// logger
-			Printer logging = me.mLogging;
-			Message msg = queue.next(); // might block
-			if (msg != null) {
-				if (msg.target == null) {
-					// No target is a magic identifier for the quit message.
-					if (logging != null) {
-						logging.println(">>>>> Stopping loop " + msg.target
-								+ " " + msg.callback + ": " + msg.what);
+    Looper me = myLooper();
+    if (me == null) {
+      throw new RuntimeException("No Looper; Looper.prepare() wasn't called on this thread.");
+    }
+    MessageQueue queue = me.mQueue;
 
-					}
-					return;
-				}
+    // Make sure the identity of this thread is that of the local process,
+    // and keep track of what that identity token actually is.
+    // Binder.clearCallingIdentity();
+    // final long ident = Binder.clearCallingIdentity();
 
-				if (logging != null) {
-					logging.println(">>>>> Dispatching to " + msg.target + " "
-							+ msg.callback + ": " + msg.what);
-				}
+    while (true) {
+      // This must be in a local variable, in case a UI event sets the
+      // logger
+      Printer logging = me.mLogging;
+      Message msg = queue.next(); // might block
+      if (msg != null) {
+        if (msg.target == null) {
+          // No target is a magic identifier for the quit message.
+          if (logging != null) {
+            logging.println(">>>>> Stopping loop " + msg.target + " " + msg.callback + ": " + msg.what);
 
-				msg.target.dispatchMessage(msg);
+          }
+          return;
+        }
 
-				if (logging != null) {
-					logging.println("<<<<< Finished to " + msg.target + " "
-							+ msg.callback);
-				}
+        if (logging != null) {
+          logging.println(">>>>> Dispatching to " + msg.target + " " + msg.callback + ": " + msg.what);
+        }
 
-				msg.recycle();
-			}
-		}
-	}
+        msg.target.dispatchMessage(msg);
 
-	/**
-	 * Return the Looper object associated with the current thread. Returns null
-	 * if the calling thread is not associated with a Looper.
-	 */
-	public static Looper myLooper() {
-		return sThreadLocal.get();
-	}
+        if (logging != null) {
+          logging.println("<<<<< Finished to " + msg.target + " " + msg.callback);
+        }
 
-	/**
-	 * Control logging of messages as they are processed by this Looper. If
-	 * enabled, a log message will be written to <var>printer</var> at the
-	 * beginning and ending of each message dispatch, identifying the target
-	 * Handler and message contents.
-	 * 
-	 * @param printer
-	 *            A Printer object that will receive log messages, or null to
-	 *            disable message logging.
-	 */
-	public void setMessageLogging(Printer printer) {
-		mLogging = printer;
-	}
+        msg.recycle();
+      }
+    }
+  }
 
-	/**
-	 * Return the {@link MessageQueue} object associated with the current
-	 * thread. This must be called from a thread running a Looper, or a
-	 * NullPointerException will be thrown.
-	 */
-	public static MessageQueue myQueue() {
-		return myLooper().mQueue;
-	}
+  /**
+   * Return the Looper object associated with the current thread. Returns null
+   * if the calling thread is not associated with a Looper.
+   */
+  public static Looper myLooper() {
+    return sThreadLocal.get();
+  }
 
-	private Looper() {
-		mQueue = new MessageQueue();
-		mRun = true;
-		mThread = Thread.currentThread();
-	}
+  /**
+   * Control logging of messages as they are processed by this Looper. If
+   * enabled, a log message will be written to <var>printer</var> at the
+   * beginning and ending of each message dispatch, identifying the target
+   * Handler and message contents.
+   * 
+   * @param printer
+   *          A Printer object that will receive log messages, or null to
+   *          disable message logging.
+   */
+  public void setMessageLogging(Printer printer) {
+    mLogging = printer;
+  }
 
-	public void quit() {
-		Message msg = new Message();
-		// NOTE: By enqueueing directly into the message queue, the
-		// message is left with a null target. This is how we know it is
-		// a quit message.
-		mQueue.enqueueMessage(msg, 0);
-	}
+  /**
+   * Return the {@link MessageQueue} object associated with the current
+   * thread. This must be called from a thread running a Looper, or a
+   * NullPointerException will be thrown.
+   */
+  public static MessageQueue myQueue() {
+    return myLooper().mQueue;
+  }
 
-	/**
-	 * Return the Thread associated with this Looper.
-	 */
-	public Thread getThread() {
-		return mThread;
-	}
+  private Looper() {
+    mQueue = new MessageQueue();
+    mRun = true;
+    mThread = Thread.currentThread();
+    
+    if (DEBUG_LOOPER)
+      mLogging = new LogPrinter(Log.DEBUG, TAG);
 
-	/** @hide */
-	public MessageQueue getQueue() {
-		return mQueue;
-	}
+  }
 
-	public void dump(Printer pw, String prefix) {
-//		pw = PrefixPrinter.create(pw, prefix);
-//		pw.println(this.toString());
-//		pw.println("mRun=" + mRun);
-//		pw.println("mThread=" + mThread);
-//		pw.println("mQueue=" + ((mQueue != null) ? mQueue : "(null"));
-//		if (mQueue != null) {
-//			synchronized (mQueue) {
-//				long now = SystemClock.uptimeMillis();
-//				Message msg = null;
-//				int i = 0;
-//				for (; i < mQueue.getSize(); i++) {
-//					msg = mQueue.getMessage(i);
-//					pw.println("  Message " + i + ": " + msg.toString(now));
-//				}
-//				pw.println("(Total messages: " + (i + 1) + ")");
-//			}
-//		}
-	}
+  public void quit() {
+    Message msg = new Message();
+    // NOTE: By enqueueing directly into the message queue, the
+    // message is left with a null target. This is how we know it is
+    // a quit message.
+    mQueue.enqueueMessage(msg, 0);
+  }
 
-	public String toString() {
-		return "Looper{" + Integer.toHexString(System.identityHashCode(this))
-				+ "}";
-	}
+  /**
+   * Return the Thread associated with this Looper.
+   */
+  public Thread getThread() {
+    return mThread;
+  }
 
-	/**
-	 * @hide
-	 */
-	public static interface Profiler {
-		void profile(Message message, long wallStart, long wallTime,
-				long threadStart, long threadTime);
-	}
+  /** @hide */
+  public MessageQueue getQueue() {
+    return mQueue;
+  }
+
+  public void dump(Printer pw, String prefix) {
+    //		pw = PrefixPrinter.create(pw, prefix);
+    //		pw.println(this.toString());
+    //		pw.println("mRun=" + mRun);
+    //		pw.println("mThread=" + mThread);
+    //		pw.println("mQueue=" + ((mQueue != null) ? mQueue : "(null"));
+    //		if (mQueue != null) {
+    //			synchronized (mQueue) {
+    //				long now = SystemClock.uptimeMillis();
+    //				Message msg = null;
+    //				int i = 0;
+    //				for (; i < mQueue.getSize(); i++) {
+    //					msg = mQueue.getMessage(i);
+    //					pw.println("  Message " + i + ": " + msg.toString(now));
+    //				}
+    //				pw.println("(Total messages: " + (i + 1) + ")");
+    //			}
+    //		}
+  }
+
+  public String toString() {
+    return "Looper{" + Integer.toHexString(System.identityHashCode(this)) + "}";
+  }
+
+  /**
+   * @hide
+   */
+  public static interface Profiler {
+    void profile(Message message, long wallStart, long wallTime, long threadStart, long threadTime);
+  }
 }
