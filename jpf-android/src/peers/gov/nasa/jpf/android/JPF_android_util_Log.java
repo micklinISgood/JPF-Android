a9e97b3ce5771fe20ce1ede313535873daf41a0e
heila <heilamvdm@gmail.com>
2013-05-29 13:37:38 +0200

    Adding verbose logs

--- jpf-android/src/peers/gov/nasa/jpf/android/JPF_android_util_Log.java
+++ jpf-android/src/peers/gov/nasa/jpf/android/JPF_android_util_Log.java
@@ -15,44 +15,42 @@ import android.util.Log;
  * 
  */
 public class JPF_android_util_Log {
-	static Logger log = JPF.getLogger("gov.nasa.jpf.android");
+  static Logger log = JPF.getLogger("gov.nasa.jpf.android");
 
-	public static int println_native(MJIEnv env, int clsObjRef, int v0,
-			int type, int stag, int smsg) {
+  public static int println_native(MJIEnv env, int clsObjRef, int v0, int type, int stag, int smsg) {
 
-		String msg = env.getStringObject(smsg);
-		String tag = env.getStringObject(stag);
+    String msg = env.getStringObject(smsg);
+    String tag = env.getStringObject(stag);
 
-		switch (type) {
-		case Log.INFO:
-			log.info(tag + ": " + msg);
-			break;
-		case Log.DEBUG:
-			log.fine(tag + ": " + msg);
-			break;
-		case Log.ERROR:
-			log.severe(tag + ": " + msg);
-			break;
-		case Log.WARN:
-			log.warning(tag + ": " + msg);
-			break;
-		case Log.VERBOSE:
-			log.fine(tag + ": " + msg);
-			break;
-		case Log.ASSERT:
-			log.fine(tag + ": " + msg);
-			break;
-		default:
-			log.fine(tag + ": " + msg);
-		}
+    switch (type) {
+    case Log.INFO:
+      log.info(tag + ": " + msg);
+      break;
+    case Log.DEBUG:
+      log.fine(tag + ": " + msg);
+      break;
+    case Log.ERROR:
+      log.severe(tag + ": " + msg);
+      break;
+    case Log.WARN:
+      log.warning(tag + ": " + msg);
+      break;
+    case Log.VERBOSE:
+      log.fine(tag + ": " + msg);
+      break;
+    case Log.ASSERT:
+      log.fine(tag + ": " + msg);
+      break;
+    default:
+      log.fine(tag + ": " + msg);
+    }
 
-		return msg.length();
-	}
+    return msg.length();
+  }
 
-	public static boolean isLoggable(MJIEnv env, int clsObjRef, int rString0,
-			int v1) {
-		boolean v = true;
-		return v;
-	}
+  public static boolean isLoggable(MJIEnv env, int clsObjRef, int rString0, int v1) {
+    boolean v = true;
+    return v;
+  }
 
 }
