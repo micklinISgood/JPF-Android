31db4c8353257314521226bff39e51973c320701
heila <heilamvdm@gmail.com>
2013-11-12 13:51:20 -0800

    Changed to Property Listener

--- jpf-android/src/main/gov/nasa/jpf/android/checkpoint/ChecklistPublisher.java
+++ jpf-android/src/main/gov/nasa/jpf/android/checkpoint/ChecklistPublisher.java
@@ -1,18 +1,19 @@
 package gov.nasa.jpf.android.checkpoint;
 
-import gov.nasa.jpf.JPF;
-import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.PropertyListenerAdapter;
+import gov.nasa.jpf.android.ResultPublishListener;
 import gov.nasa.jpf.report.Publisher;
 import gov.nasa.jpf.report.PublisherExtension;
 import gov.nasa.jpf.search.Search;
 import gov.nasa.jpf.util.script.AndroidScriptEnvironment;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.ThreadList;
+import gov.nasa.jpf.vm.VM;
 
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Stores a list of violated {@link CheckList}. Publishes a report of which
@@ -22,60 +23,50 @@ import java.util.logging.Logger;
  * @date 2 June 2013
  * 
  */
-public class ChecklistPublisher extends ListenerAdapter implements PublisherExtension {
-  private static final String TAG = ChecklistPublisher.class.getSimpleName();
-  private static Logger logger = JPF.getLogger(ChecklistPublisher.class.getName());
+public class ChecklistPublisher extends PropertyListenerAdapter implements PublisherExtension {
 
   private final static int REASON_NOT_COMPLETE = 0;
   private final static int REASON_MATCH_FAILED = 1;
 
+  /** Stores list of violating Checklists */
   private List<ChecklistViolation> violatingChecklists = new LinkedList<ChecklistViolation>();
-  private List<ChecklistCompleted> completedChecklists = new LinkedList<ChecklistCompleted>();
 
+  /** Reports checklists that where violated */
   private ChecklistReporter reporter;
-  private AndroidScriptEnvironment scriptEnv;
+
+  /** st true when violation occurs */
+  private boolean violation = false;
+
+  private boolean published = false;
 
   private Search search;
 
   public ChecklistPublisher(ChecklistReporter reporter, AndroidScriptEnvironment scriptEnv) {
     this.reporter = reporter;
-    this.scriptEnv = scriptEnv;
   }
 
-  private void printCompletions(PrintWriter pw) {
-    for (ChecklistCompleted violation : completedChecklists) {
-      pw.println("Checklist Name: " + violation.getName());
-      pw.println("Completed: " + violation.isCompleted());
-      pw.println("EventID: " + violation.checklist.getPath().eventID + " PathID: "
-          + violation.checklist.getPath().pathID);
-      pw.println("Checklist:" + violation.getChecklist());
-      pw.println("Script Events:" + violation.scriptActions);
-      pw.println();
-    }
+  public boolean check(Search search, VM vm) {
+    return !violation;
   }
 
   private void printViolations(PrintWriter pw) {
     for (ChecklistViolation violation : violatingChecklists) {
       pw.println("Checklist Name: " + violation.getName());
-      pw.println("EventID: " + violation.checklist.getPath().eventID + " PathID: "
-          + violation.checklist.getPath().pathID);
+      pw.println("EventID: " + violation.checklistInstance.getPath().eventID + " PathID: "
+          + violation.checklistInstance.getPath().pathID);
       pw.println("Checklist:" + violation.getChecklist());
       pw.print("Reason: ");
       if (violation.reason == REASON_MATCH_FAILED)
         pw.println(printMatchFailed(violation));
       else
         pw.println(printNotComplete(violation));
-
-      pw.println("Script Events:" + violation.scriptActions);
-      pw.println("Thread state:\n\t" + violation.threadState);
-      pw.println();
     }
   }
 
   private String printMatchFailed(ChecklistViolation violation) {
     StringBuilder s = new StringBuilder();
-    Checklist cl = violation.checklist.getChecklist();
-    ChecklistInstance cli = violation.checklist;
+    Checklist cl = violation.checklist;
+    ChecklistInstance cli = violation.checklistInstance;
     CheckpointInstance ins = cli.checkpointsMatched[cli.getIndex()];
     Checkpoint cp = cl.getCheckpoint(cli.getIndex());
     s.append("\nFailed because checkpoint reached (" + ins.getName() + ", tn=" + ins.getThreadAlias()
@@ -86,58 +77,58 @@ public class ChecklistPublisher extends ListenerAdapter implements PublisherExte
 
   private String printNotComplete(ChecklistViolation violation) {
     StringBuilder s = new StringBuilder();
-    Checklist cl = violation.checklist.getChecklist();
-    Checkpoint cp = cl.getCheckpoint(violation.checklist.getIndex());
+    Checklist cl = violation.checklist;
+    Checkpoint cp = cl.getCheckpoint(violation.checklistInstance.getIndex());
     s.append("Checkpoint  (" + ((cp.isNegative()) ? "!" : "") + cp.getName()
         + ", tn=?) not visited before search ended.");
     return s.toString();
   }
 
   public void addViolatingChecklist(ChecklistInstance checklist, int reason) {
-    String actions = getActions();
     String threadString = getThreadState();
 
-    violatingChecklists.add(new ChecklistViolation(checklist, actions, threadString, reason));
-
+    violatingChecklists.add(new ChecklistViolation(checklist, threadString, reason));
+    violation = true;
   }
 
-  public void addCompletedChecklist(ChecklistInstance checklist) {
-    String actions = getActions();
-    completedChecklists.add(new ChecklistCompleted(checklist, actions));
-
+  @Override
+  public String getErrorMessage() {
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    printViolations(pw);
+    //TOD fix
+    return sw.toString();
   }
 
   @Override
   public void publishPropertyViolation(Publisher publisher) {
-    PrintWriter pw = publisher.getOut();
-    publisher.publishTopicStart("checklist completions " + publisher.getLastErrorId());
-    printCompletions(pw);
-    publisher.publishTopicStart("checklist violations " + publisher.getLastErrorId());
-    printViolations(pw);
+    if (violatingChecklists.size() > 0) {
+      PrintWriter pw = publisher.getOut();
+      publisher.publishTopicStart("checklist violations " + publisher.getLastErrorId());
+      printViolations(pw);
+      published = true;
 
-
-  }
-
-  @Override
-  public void publishConstraintHit(Publisher publisher) {
-    PrintWriter pw = publisher.getOut();
-    publisher.publishTopicStart("checklist completions " + publisher.getLastErrorId());
-    printCompletions(pw);
-    publisher.publishTopicStart("checklist violations " + publisher.getLastErrorId());
-    printViolations(pw);
-
-
+    }
   }
 
   @Override
   public void publishFinished(Publisher publisher) {
-    PrintWriter pw = publisher.getOut();
-    publisher.publishTopicStart("checklist completions " + publisher.getLastErrorId());
-    printCompletions(pw);
-    publisher.publishTopicStart("checklist violations " + publisher.getLastErrorId());
-    printViolations(pw);
-   
+    if (violatingChecklists.size() > 0) {
 
+      if (!published) {
+        PrintWriter pw = publisher.getOut();
+
+        for (PublisherExtension e : publisher.getExtensions()) {
+          if (e instanceof ResultPublishListener) {
+            e.publishPropertyViolation(publisher);
+          }
+
+        }
+
+        publisher.publishTopicStart("checklist violations " + publisher.getLastErrorId());
+        printViolations(pw);
+      }
+    }
   }
 
   @Override
@@ -154,15 +145,10 @@ public class ChecklistPublisher extends ListenerAdapter implements PublisherExte
 
     if (search.isEndState()) {
       List<ChecklistInstance> lists = reporter.getFailedChecklists();
-      List<ChecklistInstance> lists2 = reporter.getCompletedChecklists();
       //add these lists to violating checklists
       this.search = search;
       for (ChecklistInstance instance : lists) {
         addViolatingChecklist(instance, REASON_NOT_COMPLETE);
-      }
-
-      for (ChecklistInstance instance : lists2) {
-        addCompletedChecklist(instance);
       }
     }
   }
@@ -182,99 +168,83 @@ public class ChecklistPublisher extends ListenerAdapter implements PublisherExte
     return out;
   }
 
-  private String getActions() {
-    return scriptEnv.getActions();
-  }
-
   protected static class ChecklistViolation {
-    ChecklistInstance checklist;
-    String scriptActions;
+
+    ChecklistInstance checklistInstance;
+    Checklist checklist;
+    CheckpointInstance[] matchedPoints;
     String threadState;
     int reason = -1;
 
-    public ChecklistViolation(ChecklistInstance checklist, String scriptActions, String threadState,
-        int reason) {
+    public ChecklistViolation(ChecklistInstance checklistInstance, String threadState, int reason) {
       super();
-      this.checklist = checklist;
-      this.scriptActions = scriptActions;
+      this.checklistInstance = checklistInstance;
+      this.checklist = this.checklistInstance.getChecklist();
+      this.matchedPoints = this.checklistInstance.checkpointsMatched;
       this.threadState = threadState;
       this.reason = reason;
-      
-         }
+
+      //stores the current index and size of checklist
+      int currentIndex = checklistInstance.getIndex();
+      int size = this.checklist.size();
+
+      if (reason == REASON_MATCH_FAILED) {
+        // this only happens when a negative checkpoint is reached, 
+        // now we search for this point that was reached
+
+        for (int i = currentIndex + 1; i < size; i++) {
+          if (checklist.getCheckpoint(i).isNegative() && matchedPoints[i] != null) {
+            checklistInstance.setIndex(i);
+            currentIndex = i;
+            break;
+          }
+        }
+
+      } else {
+        // this happens when a checkpoint was not reach before the search ended.
+        // this checkpoint can be after some negative checkpoints, so we need
+        // to search for this first positive point after the current point
+
+        for (int i = currentIndex + 1; i < size; i++) {
+          if (!checklist.getCheckpoint(i).isNegative()) {
+            checklistInstance.setIndex(i);
+            currentIndex = i;
+            break;
+          }
+        }
+
+      }
+
+    }
 
     public String getName() {
-      return this.checklist.getChecklist().getName();
+      return this.checklistInstance.getChecklist().getName();
     }
 
     public String getChecklist() {
-      
-      for (int i = checklist.getIndex() + 1; i < checklist.getChecklist().size(); i++) {
-        if (checklist.getChecklist().getCheckpoint(i).isNegative()&&checklist.checkpointsMatched[i]!=null) {
-          checklist.setIndex(i);
-          break;
-        }
-      }
 
       StringBuilder sb = new StringBuilder("");
 
-      CheckpointInstance[] matched = this.checklist.checkpointsMatched;
-      for (int i = 0; i < this.checklist.getChecklist().size(); i++) {
-        Checkpoint c = this.checklist.getChecklist().getCheckpoint(i);
-        if (i == this.checklist.getIndex()) {
+      for (int i = 0; i < checklist.size(); i++) {
+        Checkpoint c = checklist.getCheckpoint(i);
+
+        if (i == checklistInstance.getIndex()) {
           sb.append("-->");
         }
 
         sb.append(" (" + ((c.isNegative()) ? "!" : "") + c.getName() + ", tn= "
-            + ((matched[i] != null) ? matched[i].getThreadName() : "?") + ")");
-        if (this.checklist.getChecklist().getConditionIndex() == i) {
+            + ((matchedPoints[i] != null) ? matchedPoints[i].getThreadName() : "?") + ")");
+
+        if (this.checklist.getConditionIndex() == i) {
           sb.append("=>");
         } else {
           sb.append(",");
         }
+
       }
       return sb.toString();
     }
 
   }
 
-  protected static class ChecklistCompleted {
-    ChecklistInstance checklist;
-    String scriptActions;
-
-    public ChecklistCompleted(ChecklistInstance checklist, String scriptActions) {
-      super();
-      this.checklist = checklist;
-      this.scriptActions = scriptActions;
-    }
-
-    public boolean isCompleted() {
-      return checklist.isCompleted();
-    }
-
-    public String getName() {
-      return this.checklist.getChecklist().getName();
-    }
-
-    public String getChecklist() {
-      StringBuilder sb = new StringBuilder("");
-
-      CheckpointInstance[] matched = this.checklist.checkpointsMatched;
-      for (int i = 0; i < this.checklist.getChecklist().size(); i++) {
-        Checkpoint c = this.checklist.getChecklist().getCheckpoint(i);
-        if (i == this.checklist.getIndex()) {
-          sb.append("-->");
-        }
-
-        sb.append(" (" + ((c.isNegative()) ? "!" : "") + c.getName() + ", tn= "
-            + ((matched[i] != null) ? matched[i].getThreadName() : "?") + ")");
-        if (this.checklist.getChecklist().getConditionIndex() == i) {
-          sb.append("=>");
-        } else {
-          sb.append(",");
-        }
-      }
-      return sb.toString();
-    }
-
-  }
 }
